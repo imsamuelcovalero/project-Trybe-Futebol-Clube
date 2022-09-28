d27c68fd9b0b73d6bcd63a67ce40b48bd4a6b268
Samuel Covalero dos Reis <reis_samuel@hotmail.com>
2022-09-28 11:34:01 -0300

    feat(Dockerfiles): Configura os Dockerfiles e o início da aplicação

---
+++ app/backend/src/database/migrations/20220928142558-create-teams.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('teams', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        field: 'created_at',
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
